f2f039f0c359e7c51e88ecca81af2f94e154262c
Plamedi Luis <plamedi.luis11@gmail.com>
2024-05-20 14:34:30 +0200

    feat(main): can parse arguments now

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,17 @@ const std = @import("std");
 const zargparse = @import("zargparse");
 const ArgParser = zargparse.ArgumentParser;
 const ParserArg = zargparse.ParserArg;
+const ArgIterator = std.process.ArgIterator;
+const ArrayList = std.ArrayList;
+const stdout = std.io.getStdOut().writer();
+
+pub fn getArgs(iter: *ArgIterator, arr: *ArrayList([]const u8)) !void {
+    _ = iter.next();
+
+    while (iter.next()) |arg| {
+        try arr.append(arg);
+    }
+}
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -10,21 +21,40 @@ pub fn main() !void {
     const allocator = arena.allocator();
 
     // using an arena allocator so we don't need the denit for ArgParser
-    var parser = try ArgParser.init(allocator);
+    var parser = ArgParser.init(allocator);
+    var iter = try ArgIterator.initWithAllocator(allocator);
+    var arr = ArrayList([]const u8).init(allocator);
 
-    try parser.add_argument(ParserArg{
+    defer {
+        parser.deinit();
+        iter.deinit();
+        arr.deinit();
+    }
+
+    try getArgs(&iter, &arr);
+
+    try parser.addArgument(ParserArg{
         .name = "--dispatcher-server",
         .help = "dispatcher host :port, by default it uses localhost:8888",
         .default = "localhost:8888",
         .action = "store",
     });
 
-    try parser.add_argument(ParserArg{
+    try parser.addArgument(ParserArg{
         .name = "repo",
         .metavar = "REPO",
         .arg_type = ParserArg.ArgType.STRING,
         .help = "path to the repository this will observe",
     });
 
-    try parser.parseArgs();
+    try parser.parseArgs(arr.items);
+
+    if (parser.getValue("dispatcher_server")) |val| {
+        var server = std.mem.splitSequence(u8, val.str, ":");
+
+        const dispatcher_host = server.next() orelse std.debug.panic("missing dispatcher host\n", .{});
+        const dispatcher_port = server.next() orelse std.debug.panic("missing dispatcher port\n", .{});
+
+        try stdout.print("\nhost is {s}\nport is {s}", .{ dispatcher_host, dispatcher_port });
+    }
 }
